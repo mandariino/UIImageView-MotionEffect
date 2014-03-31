bd8cf8c90e8f12c6772342b4e1b5429c67f83094
Oscar Dominguez <hdominguez@gmail.com>
2014-03-31 14:47:14 -0600

    Initial release

---
+++ UIImageView+Parallax.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+Parallax.h
+//  Central
+//
+//  Created by eCentral on 31/03/14.
+//  Copyright (c) 2014 Central. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (Parallax)
+-(void)addParallax;
+@end
---
+++ UIImageView+Parallax.m
@@ -0,0 +1,33 @@
+//
+//  UIImageView+Parallax.m
+//  Central
+//
+//  Created by Oscar Domínguez on 31/03/14.
+//  Copyright (c) 2014 Central. All rights reserved.
+//
+
+#import "UIImageView+Parallax.h"
+
+@implementation UIImageView (Parallax)
+-(void)addParallax{
+    
+    UIInterpolatingMotionEffect *verticalMotionEffect =
+    [[UIInterpolatingMotionEffect alloc]
+     initWithKeyPath:@"center.y"
+     type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    verticalMotionEffect.minimumRelativeValue = @(-10);
+    verticalMotionEffect.maximumRelativeValue = @(10);
+    
+    UIInterpolatingMotionEffect *horizontalMotionEffect =
+    [[UIInterpolatingMotionEffect alloc]
+     initWithKeyPath:@"center.x"
+     type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+    horizontalMotionEffect.minimumRelativeValue = @(-10);
+    horizontalMotionEffect.maximumRelativeValue = @(10);
+    
+    UIMotionEffectGroup *group = [UIMotionEffectGroup new];
+    group.motionEffects = @[horizontalMotionEffect, verticalMotionEffect];
+    [self addMotionEffect:group];
+
+}
+@end
